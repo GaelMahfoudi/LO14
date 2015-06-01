c8d6f633d18497518ef5cbd8ee11da220b3de2ba
ProgMac <progm4c@gmail.com>
2015-06-01 19:00:03 +0200

    rhost.sh: fonctionne et commenté

--- rhost.sh
+++ rhost.sh
@@ -1,13 +1,43 @@
-# dossier racine de rvsh
+#===================================================================================
+# file         : rhost.sh
+# usage        : ---
+#
+# description  : fichier source de la commande rhost.
+#
+# options      : ---
+# authors      : G. MAHFOUDI & S. JUHEL
+# company      : UTT
+# version      : 1.0
+# bugs         : ---
+# notes        : ---
+# created      : ---
+# revision     : ---
+#===================================================================================
+
+
+# repertoire racine de rvsh
 ROOT="$HOME/rvsh"
 
+
+#=== function ======================================================================
+# name         : rhost
+# description  : permet d'acceder à la liste des machines 
+#				 rattachées au réseau virtuel.
+# 
+# parameters   : ---
+#===================================================================================
 rhost() {
 
-	echo "[*] VMs available on the network"
+	echo "Hosts available on the network:"
 
+	# on liste le repertoire qui contients les machines
 	list=$(ls $ROOT/host/)
+
+	# si le repertoire est vide, on le précise
     if [ -z "$list" ]; then
         echo "No VMs created"
+        
+    # sinon on affiche la liste
     else
         echo "$list"
     fi
