56a2c0531c4af94bef4296de288c841d69ff4dc3
ProgMac <progm4c@gmail.com>
2015-06-01 11:58:27 +0200

    debut des comments who.sh

--- who.sh
+++ who.sh
@@ -1,6 +1,33 @@
+#===================================================================================
+# file         : who.sh
+# usage        : ---
+#
+# description  : fichier source de la commande who.
+#
+# options      : see function ’usage’ below
+# authors      : G. MAHFOUDI & S. JUHEL
+# company      : UTT
+# version      : 1.0
+# bugs         : ---
+# notes        : ---
+# created      : ---
+# revision     : ---
+#===================================================================================
+
+
+# repertoire racine de rvsh
 ROOT="/home/rvsh"
 
 
+
+#=== function ======================================================================
+# name         : who_is_connected_on
+# description  : permet d'acceder à l'ensemble des utilisateurs connectés sur
+#                la machine virtuelle. 
+#
+# $1 - nom de la machine virtuelle
+#
+#===================================================================================
 who_is_connected_on() {
 
 	local hostname="$1"
