7723dd33a32d6cfafeb358754a3ff84ba271c8d4
Gaël <gael.mahfoudi@gail.com>
2015-06-02 13:58:51 +0200

    Ajout des commentaires pour finger

--- finger.sh
+++ finger.sh
@@ -1,7 +1,31 @@
+#!/bin/bash 
+
+# =====================================================================
+#
+#           FILE : finger.sh
+#
+#          USAGE : finger.sh [utilisateurs] [-h]
+#
+#    DESCRIPTION : Permet à l'utilisateur d'afficher les informations 
+#				   complémentaires de l'utilisateur spécifié.
+#
+#
+#         OPTION : voir fonction help_finger.
+#         AUTHOR : Gaël Mahfoudi & Simon Juhel
+# =====================================================================
 
 
 ROOT="$HOME/rvsh"
 
+
+
+# ====  FINGER  =======================================================
+#
+#        NAME : finger
+# DESCRIPTION : Permet à l'utilisateur d'afficher les informations 
+#				complémentaires de l'utilisateur spécifié.
+# PARAMETER $1: L'utilisateur spécifié.
+# =====================================================================
 
 function finger  {
     
