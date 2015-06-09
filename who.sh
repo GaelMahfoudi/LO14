78fc4bc924efc7b45d0c210d2e2ddb3c27f90a38
ProgMac <progm4c@gmail.com>
2015-06-09 19:16:46 +0200

    [commented] end

--- who.sh
+++ who.sh
@@ -1,55 +1,66 @@
-#===================================================================================
-# file         : who.sh
-# usage        : ---
+# =====================================================================
 #
-# description  : fichier source de la commande who.
+#           FILE : who.sh
 #
-# options      : ---
-# authors      : G. MAHFOUDI & S. JUHEL
-# company      : UTT
-# version      : 1.0
-# bugs         : ---
-# notes        : ---
-# created      : ---
-# revision     : ---
-#===================================================================================
-
-
-# repertoire racine de rvsh
-ROOT="/home/rvsh"
-
-
-
-#=== function ======================================================================
-# name         : who_is_connected_on
-# description  : permet d'acceder à l'ensemble des utilisateurs connectés sur
-#                la machine virtuelle. 
+#          USAGE : ---
 #
-# parameters   :
-# $1 - nom de la machine virtuelle
-#===================================================================================
+#    DESCRIPTION : fichier source de la commande who
+#
+#
+#         OPTION : ---
+#         AUTHOR : Gaël Mahfoudi & Simon Juhel
+#
+# =====================================================================
+
+
+# === GLOBAL VARIABLES ================================================
+
+ROOT="$HOME/rvsh"
+
+# =====================================================================
+
+
+# === FUNCTION ========================================================
+#
+#        name: who_is_connected_on
+# description: affiche l'ensemble des utilisateurs connectés à la
+#			   machine passée en argument
+# 
+#  parameters:
+#	$1 - nom de la machine
+#
+# =====================================================================
 who_is_connected_on() {
 
 	local hostname="$1"
 	
+	# si la machine a laquelle on souhaite se connecter
+	# n'existe pas
 	if [ ! -d $ROOT/host/$hostname ]; then
 		echo "Host $hostname does not exist"
-		
+	
+	# dans le cas ou la machine existe
 	else
-
-		
-		# on parcours l'ensemble des fichiers de connexion de la
-		# machine
-
+	
+		# on recupère l'ensemble des fichiers de connexion de la
+		# machine qu'on stocke dans la variable ret
 		ret=$(ls $ROOT/host/$hostname/*.tmp 2> /dev/null)
+		
+		# si personne n'est connectée
+		# on quitte la fonction
 		if [ -z "$ret" ]; then
+
 			return
+		
+		# sinon on va afficher l'ensemble de ses utilisateurs
+		# connectée 
 		else
 
 			printf "(host) $hostname\n"
 			printf "%-12s %-9s %-9s\n" "user" "hour" "date"
 			echo   "------------ --------  --------"
 
+			# on lit un à un les fichiers de connexions
 			for conn in $ret; do
 
 				# affichage formate des connexions trouvees dans le fichier
@@ -60,5 +71,10 @@ who_is_connected_on() {
 		fi
 
 		printf "\n"
+	
 	fi
+
 }
+
+
+# === END OF FILE =====================================================
