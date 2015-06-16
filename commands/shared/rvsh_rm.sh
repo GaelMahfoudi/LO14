0f461ad0b8f33382a9e9a731c19b3f73885936d6
Gaël <gael.mahfoudi@gail.com>
2015-06-16 13:46:43 +0200

    Ajout de commentaire

--- commands/shared/rvsh_rm.sh
+++ commands/shared/rvsh_rm.sh
@@ -22,18 +22,18 @@ rvsh_rm() {
 	if 	[ "$(echo $location/$dir_name | awk -F "/" '{print $(NF-1)}')" = "host" ] || \
 		[ "$(echo $location/$dir_name | awk -F "/" '{print $(NF-1)}')" = "users" ] || \
 		[ "$(echo $location/$dir_name | awk -F "/" '{print $(NF-1)}')" = "sys" ]
-	then
+	then	#On vérifie que l'on ne suprimme pas des dossiers ou fichiers systèmes
 		echo "You cannot remove $location$dir_name"
 		return
 	fi
 
-	if [ "$user" = "admin" ]
+	if [ "$user" = "admin" ]		#Si on est l'administrateur
 	then
 
 		case $location in
-			"/rvsh" )
+			"/rvsh" ) #Si on appelle la commande depuis le dossier rvsh
 					if [ "$dir_name" = "host" -o "$dir_name" = "users" -o "$dir_name" = "sys/logs" -o "$dir_name" = "sys/help" ]
-					then 
+					then  #On vérifie que l'on ne supprime pas un host ou un user ou l'aide ou les logs
 						echo "You cannot remove $dir_name"
 					else
 						if [ \( -d ${HOME}${location}/$(echo ${dir_name} | awk -F' ' '{print $1}') \) -o \
@@ -46,17 +46,17 @@ rvsh_rm() {
 					fi
 				;;
 
-			"/rvsh/host" | "/rvsh/users" )
+			"/rvsh/host" | "/rvsh/users" ) #Si on appelle la commande depuis le dossier rvsh/host ou /users
 					echo "You cannot remove directories from $location"
 				;;
 
-			*		)
+			*		)						#Si on appelle la commande depuis un autre dossier
 
 					if [  "$(echo $location | awk -F "/" '{print $(NF-1)}')" != "host" -a "$(echo $location | awk -F "/" '{print $(NF-1)}')" != "users" ]
-					then
+					then		#On vérifie que l'on ne supprime pas un host ou un user
 						if [ \( -d ${HOME}${location}/$(echo ${dir_name} | awk -F' ' '{print $1}') \) -o \
 						     \( -e ${HOME}${location}/$(echo ${dir_name} | awk -F' ' '{print $1}') \) ]
-						then
+						then	#Si le dossier existe on le suprimme
 							rm -r ${HOME}${location}/$(echo ${dir_name} | awk -F' ' '{print $1}')
 						else
 							echo "No such a directory"
@@ -70,7 +70,7 @@ rvsh_rm() {
 	else
 		if [ \( -d ${HOME}${location}/$(echo ${dir_name} | awk -F' ' '{print $1}') \) -o \
 						     \( -e ${HOME}${location}/$(echo ${dir_name} | awk -F' ' '{print $1}') \) ]
-		then
+		then 	#L'utilisateur peut supprimer n'importe quel dossier/fichier de son arborescence. Si celui ci existe.
 			rm -r ${HOME}${location}/$(echo ${dir_name} | awk -F' ' '{print $1}')
 		else
 			echo "No such a directory"
